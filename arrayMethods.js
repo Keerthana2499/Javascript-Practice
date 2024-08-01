04759418598c976ba19642051ab840efe3b1048f
Keerthu1799 <kerthanamanohoran2024@gmail.com>
2024-08-01 21:30:06 +0530

    array, string , methods

---
+++ arrayMethods.js
@@ -0,0 +1,31 @@
+// let myArray = ['gopal', 'Ram', 'Micheal'];
+// let myArray1 = ['gopal', 'Ram', 'Micheal'];
+
+// let numbers = [ 3, 4, 5, 6, 7,1, 2, 8]
+
+// let res= myArray.includes('Ramu');
+// let res= myArray.concat(myArray1);
+// let res= myArray.join(',');
+// let res= myArray.slice(0,2) 
+// let res= myArray.splice(0,2, 'Mathi', 'Vicky');
+// let res= myArray.push("Niranjan");
+// let res= myArray.pop();
+// let res= myArray.shift();
+// let res= myArray.unshift('Sowmiya');
+// numbers.map((a) => {
+//     console.log(a)
+// })
+// let res= numbers.map((a) => a*2);
+// let res= numbers.filter((a)=>a%2==0 );
+// let res= numbers.find((a)=>a%2==0 );
+// let res= numbers.every((a)=>a%2==0 );
+// let res= numbers.some((a)=>a%2==0 );
+// let res= numbers.reduce((a,b)=>a+b);
+// let res= numbers.sort((a,b)=>a-b);
+// let res= numbers.fill(10,2);
+// let res= numbers.reverse();
+
+// console.log(res);
+// console.log(res);
+// console.log(myArray);
+// console.log(typeof(res))
---
+++ stringMethods.js
@@ -0,0 +1,34 @@
+// let a = "Falooda";
+let a = "7449143583";
+// let a = "Falooda , fullfils everything - Falooda";
+// let res = a.trimStart();
+// let res = a.trimEnd();
+// let res = a.trim()
+// let res = a.startsWith('L');
+// let res = a.endsWith('a');
+// let res = a.includes('z');
+// let res = a.indexOf('o')
+// let res = a.lastIndexOf('o');
+// let res = a.charAt('F');
+// let res = a.charCodeAt('F');
+// let res = a.replace('Falooda','Icecream')
+// let res = a.replaceAll('Falooda','Icecream')
+// let res = a.split(',')
+// let res = a.toLowerCase();
+// let res = a.toUpperCase();
+// let res = a.match('a')
+// let res = a.matchAll('a')
+// let res = a.padStart(14, '+91-')
+// let res = a.padEnd(14, '+91-')
+// let res = a.slice(5,8)
+// let res = a.substring(0,5);
+
+
+
+
+
+
+// let res = a.length;
+// console.log(a.length);
+
+console.log(res)
